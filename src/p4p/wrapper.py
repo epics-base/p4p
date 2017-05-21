b3206f1d846e8df72c8551e53d221417b0e01efa
Michael Davidsaver <mdavidsaver@gmail.com>
2017-05-20 21:35:34 -0400

    Add helpers or defining struct/union

--- src/p4p/wrapper.py
+++ src/p4p/wrapper.py
@@ -5,3 +5,15 @@ from ._p4p import Type, Value
 from ._p4p import clearProviders
 
 atexit.register(clearProviders)
+
+def Struct(spec=None, id=None):
+    return ('S', id, spec)
+
+def Union(spec=None, id=None):
+    return ('U', id, spec)
+
+def StructArray(spec=None, id=None):
+    return ('aS', id, spec)
+
+def UnionArray(spec=None, id=None):
+    return ('aU', id, spec)
