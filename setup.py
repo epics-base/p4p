10ffed9ac193c461952aa999b68139acfcd56874
Michael Davidsaver <mdavidsaver@gmail.com>
2018-10-18 22:55:08 -0700

    1.1a36

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ ext = Extension(
 
 setup(
     name='p4p',
-    version='1.1a35',
+    version='1.1a36',
     description="Python interface to PVAccess protocol client",
     url='https://mdavidsaver.github.io/p4p',
     author='Michael Davidsaver',
