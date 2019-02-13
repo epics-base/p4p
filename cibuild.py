c64d21b93406cfc743b3cd226aedb1ace84ad28e
Michael Davidsaver <mdavidsaver@gmail.com>
2019-02-12 17:48:22 -0800

    appveyor

--- cibuild.py
+++ cibuild.py
@@ -83,7 +83,7 @@ def build(args):
     call_py(['-m', 'change_tag', '--rm', '--tag', tag, results[0]])
 
 def upload(args):
-    if 'APPVEYOR_PULL_REQUEST_NUMBER' in os.environ and 'TWINE_USERNAME' in os.environ:
+    if 'APPVEYOR_PULL_REQUEST_NUMBER' in os.environ or 'TWINE_USERNAME' not in os.environ:
         print("APPVEYOR is PR, skip upload attempt")
         return
 
