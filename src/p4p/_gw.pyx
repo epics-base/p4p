5574f4c6b8116b3c43a707698831f5c5b64987ff
Michael Davidsaver <mdavidsaver@gmail.com>
2019-11-24 20:29:21 -0800

    gw: skip auto_pickle
    not needed

--- src/p4p/_gw.pyx
+++ src/p4p/_gw.pyx
@@ -135,6 +135,7 @@ cdef extern from "gwchannel.h" nogil:
 
 GWProvider.prepare()
 
+@cython.auto_pickle(False)
 cdef class ClientInstaller(object):
     cdef string name
     cdef weak_ptr[ChannelProvider] provider
@@ -150,6 +151,7 @@ cdef class ClientInstaller(object):
         with nogil:
             ChannelProviderRegistry.clients().get().remove(self.name)
 
+@cython.auto_pickle(False)
 cdef class Client(object):
     """Client(provider, config)
     GW Client.  Wraps a C++ class ChannelProvider.  Pass to `Provider` ctor.
@@ -183,6 +185,7 @@ cdef class Client(object):
         inst.provider = <weak_ptr[ChannelProvider]>self.provider
         return inst
 
+@cython.auto_pickle(False)
 cdef class InfoBase(object):
     cdef shared_ptr[const PeerInfo] info
 
@@ -219,6 +222,7 @@ cdef class InfoBase(object):
         with nogil:
             self.info.reset()
 
+@cython.auto_pickle(False)
 cdef class CreateOp(InfoBase):
     """Handle for in-progress Channel creation request
     """
@@ -254,6 +258,7 @@ cdef class CreateOp(InfoBase):
             raise RuntimeError("Dead CreateOp")
 
 
+@cython.auto_pickle(False)
 cdef class Channel(InfoBase):
     """Wraps C++ class GWChan
 
@@ -300,6 +305,7 @@ cdef class Channel(InfoBase):
             ch.get().disconnect()
 
 @cython.no_gc_clear
+@cython.auto_pickle(False)
 cdef class Provider:
     """Provider(name, client, handler)
     GW Server endpoint.  wrapper for C++ class GWProvider
