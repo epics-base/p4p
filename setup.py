5f7483a9bb6c6da297e2663a6e33e162963d2409
Michael Davidsaver <mdavidsaver@gmail.com>
2020-05-18 12:58:04 -0700

    3.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import epicscorelibs.version
 from epicscorelibs.config import get_config_var
 
 # the following line is matched from cibuild.py
-package_version = '3.5.0a2'
+package_version = '3.5.0'
 
 cxxflags = []
 ldflags = []
