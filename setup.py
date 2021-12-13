4557e091abb2f196db5a12dda1a9e7c79a55d0ba
Michael Davidsaver <mdavidsaver@gmail.com>
2021-12-12 22:15:17 -0800

    4.0.0a4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import pvxslibs.path
 import pvxslibs.version
 
 # the following line is matched from cibuild.py
-package_version = '4.0.0a3'
+package_version = '4.0.0a4'
 
 cxxflags = ['-std=c++11']
 ldflags = []
