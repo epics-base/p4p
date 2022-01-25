f98459861279dd326cde26c6d82cf1461775fec2
Michael Davidsaver <mdavidsaver@gmail.com>
2022-01-25 09:33:36 -0800

    py 3.10
    asyncio not yet supported due to loop= removal

--- src/p4p/test/test_asyncio.py
+++ src/p4p/test/test_asyncio.py
@@ -1,10 +1,10 @@
 import sys
 import unittest
 
-if sys.version_info<(3,4):
+if sys.version_info<(3,4) or sys.version_info>=(3,10):
     class TestDummy(unittest.TestCase):
         def test_asyncio(self):
-            raise unittest.SkipTest("asyncio needs Py >=3.4")
+            raise unittest.SkipTest("asyncio needs Py >=3.4.  >=3.10 not yet supported")
 
 else:
     from .asynciotest import *
