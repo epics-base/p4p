706602f2761c5fcc9ac01d007e65d581f2e7b282
Ivan Finch <ivan.finch@stfc.ac.uk>
2024-08-25 17:20:06 +0100

    Most of the restore work can be done in the handler open()

--- example/persist.py
+++ example/persist.py
@@ -1,20 +1,25 @@
 """
 Use a handler to automatically persist values to an SQLite3 file database.
-Any values persisted this way will be automatically restored when the 
-program is rerun.
+Any values persisted this way will be automatically restored when the
+program is rerun. The details of users who
 
-There are is an important caveat for this simple demo:
-The persist_handler will not work as expected if anything other than the 
+Try monitoring the PV `demo:pv:optime` then quit, wait, and restart the
+program while continuing to monitor the PV. Compare with the value of
+`demo:pv:uptime` which resets on each program start. Try setting the value of
+demo:pv:optime while continuing to monitor it. It is recommended to
+inspect the persisted file, e.g. `sqlite3 persist_pvs.db "select * from pvs"`.
+
+There is an important caveat for this simple demo:
+The `PersistHandler` will not work as expected if anything other than the
 value of a field is changed, e.g. if a Control field was added to an NTScalar
-if would not be persisted correctly. This could be resolved by correctly 
-merging the pv.current() and value appropriately.
+if would not be persisted correctly. This could be resolved by correctly
+merging the pv.current().raw and value.raw appropriately in the post()
+and put().
 """
 
 import json
-import random
 import sqlite3
 import time
-from typing import Any, Tuple
 
 from p4p import Value
 from p4p.nt.scalar import NTScalar
@@ -23,111 +28,143 @@ from p4p.server.raw import Handler
 from p4p.server.thread import SharedPV
 
 
-class persist_handler(Handler):
+class PersistHandler(Handler):
     """
     A handler that will allow simple persistence of values and timestamps
     across retarts. It requires a post handler in order to persist values
     set within the program.
     """
+
     def __init__(self, pv_name: str, conn: sqlite3.Connection):
         self._conn = conn
         self._pv_name = pv_name
 
-    def post(self, pv: SharedPV, value: Value):
-        # Always update the timestamp
-        if value.changed():
+    def open(self, value, **kws):
+        # If there is a value already in the database we always use that
+        # instead of the supplied initial value, unless the
+        # handler_open_restore flag indicates otherwise.
+        if not kws.pop("handler_open_restore", True):
+            return
+
+        res = self._conn.execute(
+            "SELECT data FROM pvs WHERE id=?", [self._pv_name]
+        )
+        query_val = res.fetchone()
+
+        if query_val is not None:
+            # We could, in theory, re-apply authentication here based on the
+            # account and peer information.
+            json_val = json.loads(query_val[0])
+            print(f"Will restore to {self._pv_name} value: {json_val['value']}")
+
+            # Override initial value
+            value["value"] = json_val["value"]
+
+            value["timeStamp.secondsPastEpoch"] = json_val["timeStamp"][
+                "secondsPastEpoch"
+            ]
+            value["timeStamp.nanoseconds"] = json_val["timeStamp"][
+                "nanoseconds"
+            ]
+        else:
+            self._upsert(value)
+
+    def _update_timestamp(self, value):
+        if not value.changed("timeStamp"):
             now = time.time()
-            value["timeStamp.secondsPastEpoch"] = int(now // 1)
+            value["timeStamp.secondsPastEpoch"] = now // 1
             value["timeStamp.nanoseconds"] = int((now % 1) * 1e9)
 
+    def _upsert(self, value, account=None, peer=None) -> None:
         # Persist the data
         val_json = json.dumps(value.todict())
 
-        cur = self._conn.cursor()
-        cur.execute(
+        # Use UPSERT: https://sqlite.org/lang_upsert.html
+        conn.execute(
             """
-                    INSERT INTO pvs (id, data) VALUES (?, ?)
-                    ON CONFLICT(id)
-                    DO UPDATE set data = ?;
-                    """,
-            [self._pv_name, val_json, val_json],
+            INSERT INTO pvs (id, data, account, peer) 
+                        VALUES (:name, :json_data, :account, :peer)
+            ON CONFLICT(id)
+            DO UPDATE SET data = :json_data, account = :account, peer = :peer;
+            """,
+            {
+                "name": self._pv_name,
+                "json_data": val_json,
+                "account": account,
+                "peer": peer,
+            },
         )
         conn.commit()
-        cur.close()
+
+    def post(self, pv: SharedPV, value: Value, **kwargs):
+        self._update_timestamp(value)
+
+        self._upsert(
+            value,
+            account=kwargs.pop("handler_post_upsert_account", None),
+            peer=kwargs.pop("handler_post_upsert_peer", None),
+        )
 
     def put(self, pv: SharedPV, op: ServerOperation):
-        pv.post(op.value())  # Triggers the post() above
+        value = op.value().raw
+
+        # The post takes care of updating the timestamp
+        pv.post(
+            op.value(),
+            handler_post_upsert_account=op.account(),
+            handler_post_upsert_peer=op.peer(),
+        )
         op.done()
-
-
-# Helper functions for restoring values on program startup
-def get_initial(pv_name: str, conn: sqlite3.Connection, default=None) -> Tuple[Any, Any]:
-    """
-    Retrieve the initial value from the SQLite database and if there isn't
-    a value then return a default value instead
-    """
-
-    cur = conn.cursor()
-    res = cur.execute("SELECT data FROM pvs WHERE id=?", [pv_name])
-    query_val = res.fetchone()
-    cur.close()
-
-    if query_val is not None:
-        json_val = json.loads(query_val[0])
-        print(f"Will restore to {pv_name} value: {json_val['value']}")
-        return json_val["value"], json_val
-
-    return default, None
-
-
-def setup_pv(name: str, type: str, conn: sqlite3.Connection, default=None) -> SharedPV:
-    """
-    Setting up these PVs with the handler and restoring their values is
-    somewhat complex!
-    """
-    initial, full_result = get_initial(name, conn, default)
-
-    timestamp = None
-    if full_result:
-        if full_result.get("timeStamp"):
-            timestamp = (
-                full_result["timeStamp"]["secondsPastEpoch"]
-                + full_result["timeStamp"]["nanoseconds"] / 1e9
-            )
-
-    return SharedPV(
-        nt=NTScalar(type),
-        handler=persist_handler(name, conn),
-        initial=initial,
-        timestamp=timestamp,
-    )
 
 
 # Create an SQLite dayabase to function as our persistence store
 conn = sqlite3.connect("persist_pvs.db", check_same_thread=False)
-cur = conn.cursor()
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS pvs (id varchar(255), data json, PRIMARY KEY (id));"
-)
-cur.close()
+# conn.execute("DROP TABLE IF EXISTS pvs")
+conn.execute(
+    "CREATE TABLE IF NOT EXISTS pvs (id VARCHAR(255), data JSON, account VARCHAR(30), peer VARCHAR(55), PRIMARY KEY (id));"
+)  # IPv6 addresses can be long and will contain port number as well!
 
-duplicate_pv = setup_pv("demo:pv:int", "i", conn, default=12)
+duplicate_pv = SharedPV(
+    nt=NTScalar("i"), handler=PersistHandler("demo:pv:int", conn), initial=12
+)
 pvs = {
-    "demo:pv:randint": setup_pv("demo:pv:randint", "i", conn, default=-1),
+    "demo:pv:optime": SharedPV(
+        nt=NTScalar("i"),
+        handler=PersistHandler("demo:pv:optime", conn),
+        initial=0,
+    ),  # Operational time; total time running
+    "demo:pv:uptime": SharedPV(
+        nt=NTScalar("i"),
+        handler=PersistHandler("demo:pv:optime", conn),
+        timestamp=time.time(),
+        initial=0,
+        handler_open_restore=False,
+    ),  # Uptime since most recent (re)start
     "demo:pv:int": duplicate_pv,
-    "demo:pv:float": setup_pv("demo:pv:float", "d", conn, default=9.99),
-    "demo:pv:string": setup_pv("demo:pv:string", "s", conn, default="Hello!"),
+    "demo:pv:float": SharedPV(
+        nt=NTScalar("d"),
+        handler=PersistHandler("demo:pv:float", conn),
+        initial=9.99,
+    ),
+    "demo:pv:string": SharedPV(
+        nt=NTScalar("s"),
+        handler=PersistHandler("demo:pv:string", conn),
+        initial="Hello!",
+    ),
     "demo:pv:alias_int": duplicate_pv,  # It works except for reporting its restore
 }
 
-print(f"Starting server with the following PVs: {pvs.keys()}")
+print(f"Starting server with the following PVs: {pvs}")
 
 server = None
 try:
     server = Server(providers=[pvs])
     while True:
         time.sleep(1)
-        pvs["demo:pv:randint"].post(random.randint(1, 1000))
+        increment_value = pvs["demo:pv:uptime"].current().raw["value"] + 1
+        pvs["demo:pv:uptime"].post(increment_value)
+        increment_value = pvs["demo:pv:optime"].current().raw["value"] + 1
+        pvs["demo:pv:optime"].post(increment_value)
 except KeyboardInterrupt:
     pass
 finally:
